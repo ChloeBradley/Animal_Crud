52b6083843e415e38c3014ade53f877ddddbb0ad
Chloe Bradley <ccbradley77@gmail.com>
2015-02-08 21:14:56 -0700

    created form and modified controller

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,8 +1,24 @@
 class AnimalsController < ApplicationController
   def index
+    @animal = Animal.all
   end
 
-def new
-  @animal = Animal.new
+  def create
+    @animal = Animal.new
+    if @animal.save
+      redirect_to animals_path
+    else
+      render :new
+    end
+  end
+
+  def new
+    @animal = Animal.new
+  end
 end
+
+private
+
+def Animal_params
+  params.require(:animal).permit(:breed, :weight, :color)
 end
